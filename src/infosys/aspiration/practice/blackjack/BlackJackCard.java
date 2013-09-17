4fc797ae691eb4c78eb89b89e9a8948599e015f8
anirban nag <nag.anirbanatwork@gmail.com>
2013-09-18 00:46:32 +0530

    change package name

--- src/infosys/aspiration/practice/blackjack/BlackJackCard.java
+++ src/infosys/aspiration/practice/blackjack/BlackJackCard.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package infosys.aspiration.test.blackjack;
+package infosys.aspiration.practice.blackjack;
 
 import java.util.HashMap;
 import java.util.Map;
