98b28f8f1cf3ffdb6eabe5cae9a08e0082d85d69
tintinmj <nag.anirbanatwork@gmail.com>
2013-09-21 02:22:38 +0530

    modify DNA structure test

--- src/infosys/aspiration/practice/dnastructure/DNAStructureTester.java
+++ src/infosys/aspiration/practice/dnastructure/DNAStructureTester.java
@@ -1,5 +1,7 @@
 package infosys.aspiration.practice.dnastructure;
 
+import infosys.aspiration.libraryutil.ArrayUtil;
+
 /**
  *
  * @author Tintinmj
@@ -26,6 +28,35 @@ public class DNAStructureTester {
         
         printDNA(2, leftDNA2, rightDNA2, 
                     test.getDNABasePositionShortcut(leftDNA2, rightDNA2));
+        
+        // TestCase 3
+        String[] leftDNA3  = new String[]{"A1", "G1", "A2", "G2", "A3", "G3", 
+                                          "A4", "G4", "A5", "G5", "A6", "G6",
+                                          "A7", "G7", "A8", "G8", "A9", "G9", 
+                                          "A10", "G10", "A11", "G11", "A12", "G12"};
+        String[] rightDNA3 = new String[]{"T1", "C1", "T2", "C2", "T3", "C3",
+                                          "T4", "C4", "T5", "C5", "T6", "C6",
+                                          "T7", "C7", "T8", "C8", "T9", "C9", 
+                                          "T10", "C10", "T11", "C11", "T12", "C12"};
+        ArrayUtil.shuffleArray(rightDNA3);
+        printDNA(3, leftDNA3, rightDNA3, 
+                    test.getDNABasePosition(leftDNA3, rightDNA3));
+        printDNA(3, leftDNA3, rightDNA3, 
+                    test.getDNABasePositionShortcut(leftDNA3, rightDNA3));
+        
+        // TestCase 4
+        String[] leftDNA4  = new String[]{"A1", "G1", "A2", "G2", "A3", "G3", 
+                                          "A4", "G4", "A5", "G5", "A6", "G6",
+                                          "A7", "G7", "A8", "G8", "A9", "G9", 
+                                          "A10", "G10", "A11", "G11", "A12", "G12"};
+        String[] rightDNA4 = new String[]{"T3", "T10", "C12", "C4", "T4", "C7",
+                                          "T2", "C11", "C10", "T7", "C8", "C1",
+                                          "T9", "T11", "T1", "C6", "C9", "C2",
+                                          "C5", "T5", "C3", "T8", "T6", "T12"};
+        printDNA(4, leftDNA4, rightDNA4, 
+                    test.getDNABasePosition(leftDNA4, rightDNA4));
+        printDNA(4, leftDNA4, rightDNA4, 
+                    test.getDNABasePositionShortcut(leftDNA4, rightDNA4));
         
     }
     
