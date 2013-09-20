9d04b0c3f51654b54576f8a3afe84066cd75e15a
anirban nag <nag.anirbanatwork@gmail.com>
2013-09-21 02:25:48 +0530

    add new methods in ArrayUtil
    add xxxToStringArray() and shuffleArray

--- src/infosys/aspiration/libraryutil/ArrayUtil.java
+++ src/infosys/aspiration/libraryutil/ArrayUtil.java
@@ -4,6 +4,7 @@
  */
 package infosys.aspiration.libraryutil;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -40,34 +41,78 @@ public final class ArrayUtil {
     }
     
     public static boolean contains(int []array, int key) {
-        String []tempArray = new String[array.length];
-        for(int i = 0; i < array.length; i++) {
-            tempArray[i] = String.valueOf(array[i]);
-        }
-        return contains(tempArray, String.valueOf(key));
+        return contains(intToStringArray(array), String.valueOf(key));
     }
     
     public static boolean contains(char []array, char key) {
+        return contains(charToStringArray(array), String.valueOf(key));
+    }
+    
+    public static boolean contains(Object []array, Object key) {
+        return contains(objectToStringArray(array), key.toString());
+    }
+    
+    public static boolean contains(double []array, double key) {
+        return contains(doubleToStringArray(array), String.valueOf(key));
+    }
+    
+    public static String[] intToStringArray(int []array) {
         String []tempArray = new String[array.length];
         for(int i = 0; i < array.length; i++) {
             tempArray[i] = String.valueOf(array[i]);
         }
-        return contains(tempArray, String.valueOf(key));
+        return tempArray;
     }
     
-    public static boolean contains(Object []array, Object key) {
+    public static String[] charToStringArray(char []array) {
+        String []tempArray = new String[array.length];
+        for(int i = 0; i < array.length; i++) {
+            tempArray[i] = String.valueOf(array[i]);
+        }
+        return tempArray;
+    }
+    
+    public static String[] objectToStringArray(Object []array) {
         String []tempArray = new String[array.length];
         for(int i = 0; i < array.length; i++) {
             tempArray[i] = (array[i]).toString();
         }
-        return contains(tempArray, key.toString());
+        return tempArray;
     }
     
-    public static boolean contains(double []array, double key) {
+    public static String[] doubleToStringArray(double []array) {
         String []tempArray = new String[array.length];
         for(int i = 0; i < array.length; i++) {
             tempArray[i] = String.valueOf(array[i]);
         }
-        return contains(tempArray, String.valueOf(key));
+        return tempArray;
+    }
+    
+    public static void shuffleArray(String []array) {
+        Random rnd = new Random();
+        for (int i = array.length - 1; i > 0; i--)
+        {
+          int index = rnd.nextInt(i + 1);
+          // Simple swap
+          String a = array[index];
+          array[index] = array[i];
+          array[i] = a;
+        }
+    }
+    
+    public static void shuffleArray(int []array) {
+        shuffleArray(intToStringArray(array));
+    }
+    
+    public static void shuffleArray(char []array) {
+        shuffleArray(charToStringArray(array));
+    }
+    
+    public static void shuffleArray(double []array) {
+        shuffleArray(doubleToStringArray(array));
+    }
+    
+    public static void shuffleArray(Object []array) {
+        shuffleArray(objectToStringArray(array));
     }
 }
